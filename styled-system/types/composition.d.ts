9dc17d5037fc85f858a352f7d6a47f04410602b6
Sean McQuaid <sean.mcquaid@cfacorp.com>
2023-11-02 11:52:02 -0400

    delete

--- styled-system/types/composition.d.ts
+++ styled-system/types/composition.d.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import type { CompositionStyleObject } from "./system-types";
-import type { Token } from "../tokens";
+import type {  CompositionStyleObject  } from './system-types';
+import type {  Token  } from '../tokens';
 
 interface Recursive<T> {
-  [key: string]: Recursive<T> | T;
+  [key: string]: Recursive<T> | T
 }
 
 /* -----------------------------------------------------------------------------
@@ -11,97 +11,97 @@ interface Recursive<T> {
  * -----------------------------------------------------------------------------*/
 
 type TextStyleProperty =
-  | "fontSize"
-  | "fontSizeAdjust"
-  | "fontVariationSettings"
-  | "fontVariantPosition"
-  | "fontVariantCaps"
-  | "fontVariantNumeric"
-  | "fontVariantAlternates"
-  | "fontVariantLigatures"
-  | "fontFamily"
-  | "fontWeight"
-  | "fontSynthesis"
-  | "fontStyle"
-  | "fontVariant"
-  | "lineHeight"
-  | "letterSpacing"
-  | "textDecoration"
-  | "textTransform"
-  | "textIndent"
-  | "textDecorationColor"
-  | "textDecorationLine"
-  | "textDecorationStyle"
-  | "textEmphasisColor"
-  | "textEmphasisPosition"
-  | "textEmphasisStyle"
-  | "hyphenateCharacter"
-  | "textOrientation"
-  | "textOverflow"
-  | "textRendering";
+  | 'fontSize'
+  | 'fontSizeAdjust'
+  | 'fontVariationSettings'
+  | 'fontVariantPosition'
+  | 'fontVariantCaps'
+  | 'fontVariantNumeric'
+  | 'fontVariantAlternates'
+  | 'fontVariantLigatures'
+  | 'fontFamily'
+  | 'fontWeight'
+  | 'fontSynthesis'
+  | 'fontStyle'
+  | 'fontVariant'
+  | 'lineHeight'
+  | 'letterSpacing'
+  | 'textDecoration'
+  | 'textTransform'
+  | 'textIndent'
+  | 'textDecorationColor'
+  | 'textDecorationLine'
+  | 'textDecorationStyle'
+  | 'textEmphasisColor'
+  | 'textEmphasisPosition'
+  | 'textEmphasisStyle'
+  | 'hyphenateCharacter'
+  | 'textOrientation'
+  | 'textOverflow'
+  | 'textRendering'
 
-export type TextStyle = CompositionStyleObject<TextStyleProperty>;
+export type TextStyle = CompositionStyleObject<TextStyleProperty>
 
-export type TextStyles = Recursive<Token<TextStyle>>;
+export type TextStyles = Recursive<Token<TextStyle>>
 
 /* -----------------------------------------------------------------------------
  * Layer styles
  * -----------------------------------------------------------------------------*/
 
 type Placement =
-  | "Top"
-  | "Right"
-  | "Bottom"
-  | "Left"
-  | "Inline"
-  | "Block"
-  | "InlineStart"
-  | "InlineEnd"
-  | "BlockStart"
-  | "BlockEnd";
+  | 'Top'
+  | 'Right'
+  | 'Bottom'
+  | 'Left'
+  | 'Inline'
+  | 'Block'
+  | 'InlineStart'
+  | 'InlineEnd'
+  | 'BlockStart'
+  | 'BlockEnd'
 
 type Radius =
-  | `Top${"Right" | "Left"}`
-  | `Bottom${"Right" | "Left"}`
-  | `Start${"Start" | "End"}`
-  | `End${"Start" | "End"}`;
+  | `Top${'Right' | 'Left'}`
+  | `Bottom${'Right' | 'Left'}`
+  | `Start${'Start' | 'End'}`
+  | `End${'Start' | 'End'}`
 
 type LayerStyleProperty =
-  | "background"
-  | "backgroundColor"
-  | "backgroundImage"
-  | "borderRadius"
-  | "border"
-  | "borderWidth"
-  | "borderColor"
-  | "borderStyle"
-  | "boxShadow"
-  | "filter"
-  | "backdropFilter"
-  | "transform"
-  | "color"
-  | "opacity"
-  | "backgroundBlendMode"
-  | "backgroundAttachment"
-  | "backgroundClip"
-  | "backgroundOrigin"
-  | "backgroundPosition"
-  | "backgroundRepeat"
-  | "backgroundSize"
+  | 'background'
+  | 'backgroundColor'
+  | 'backgroundImage'
+  | 'borderRadius'
+  | 'border'
+  | 'borderWidth'
+  | 'borderColor'
+  | 'borderStyle'
+  | 'boxShadow'
+  | 'filter'
+  | 'backdropFilter'
+  | 'transform'
+  | 'color'
+  | 'opacity'
+  | 'backgroundBlendMode'
+  | 'backgroundAttachment'
+  | 'backgroundClip'
+  | 'backgroundOrigin'
+  | 'backgroundPosition'
+  | 'backgroundRepeat'
+  | 'backgroundSize'
   | `border${Placement}`
   | `border${Placement}Width`
-  | "borderRadius"
+  | 'borderRadius'
   | `border${Radius}Radius`
   | `border${Placement}Color`
   | `border${Placement}Style`
-  | "padding"
-  | `padding${Placement}`;
+  | 'padding'
+  | `padding${Placement}`
 
-export type LayerStyle = CompositionStyleObject<LayerStyleProperty>;
+export type LayerStyle = CompositionStyleObject<LayerStyleProperty>
 
-export type LayerStyles = Recursive<Token<LayerStyle>>;
+export type LayerStyles = Recursive<Token<LayerStyle>>
 
 export interface CompositionStyles {
-  textStyles: TextStyles;
-  layerStyles: LayerStyles;
+  textStyles: TextStyles
+  layerStyles: LayerStyles
 }
