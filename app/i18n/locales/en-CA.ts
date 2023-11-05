08761e827a9d614158ab9d987d2535dc1c9acc2f
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-11-05 14:45:08 -0500

    add en-US

---
+++ app/i18n/locales/en-CA.ts
@@ -0,0 +1,10 @@
+/* eslint "sort-keys": ["warn", "asc", {"caseSensitive": false, "natural": false, "minKeys": 2}] */
+
+const enCALocale = {
+    HomePage: {
+      title: "Remix Playground",
+    },
+  } as const;
+  
+  export default enCALocale;
+  
